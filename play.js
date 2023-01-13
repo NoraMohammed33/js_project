79c6ba926f8a1e455bd4701f47df88ef7d2f6ca3
Gehad Hashim <gehad7hashim@outlook.com>
2023-01-13 10:35:47 +0200

    add sound on football game

--- play.js
+++ play.js
@@ -1,29 +1,45 @@
-// document.write("kickk");
-var kicker=document.getElementById("player");
-var ball=document.getElementById("ball");
+
+ var kicker=document.getElementById("player");
+ var ball=document.getElementById("ball");
+
 
 function move()
 {
 ball.style.animation="ballMove 3s";
 }
+
 var score=0;
+
 document.getElementsById("dispScore").innerText=1;
 function drag(event) {
-   event.dataTransfer.setData('text',event.target.id);
- }
+    event.dataTransfer.setData('text',event.target.id);
+  }
 
- function allow (event) {
-   event.preventDefault();
- }
- 
+  function allow (event) {
+    event.preventDefault();
+  }
+  
 
- function drop(event) {
-   event.preventDefault();
-   var gol= event.dataTransfer.getData('text');
-   alert("Goal!")
-   document.getElementById("status").innerText="Bravo!";
+  function drop(event) {
+    event.preventDefault();
+    var gol= event.dataTransfer.getData('text');
+    alert("Goal!")
+    document.getElementById("status").innerText="Bravo!";
 
-   score=score+1;
- }
+    score=score+1;
+  }
 
- 
+  //----------------- FB sound -------------------------------------
+  
+  function FBsound_play()
+  {
+   document.getElementById("FBsound").play();
+   document.getElementById("FBsound-img").src='Muted.jpg';
+  }
+
+  
+  function FBsound_stop()
+  {
+   document.getElementById("FBsound").pause();
+   document.getElementById("FBsound-img").src='unMuted.jpg';
+  }
