3b9628ebf41f7c3ba7364633e594ff944227be8d
Gehad Hashim <gehad7hashim@outlook.com>
2023-01-17 20:22:35 +0200

    update on football game

--- Football/play.js
+++ Football/play.js
@@ -2,39 +2,45 @@
  var kicker=document.getElementById("player");
  var ball=document.getElementById("ball");
 
-
+var stus=document.getElementById("status");
 function move()
 {
 ball.style.animation="ballMove 3s";
+
+
 }
+function moveUp()
+{
+  ball.style.animation="ballMoveUp 3s";
 
+}
+function moveDown()
+{
+  ball.style.animation="ballMoveDown 3s";
+
+}
+var arrUp=document.getElementById("arrow-up");
+var arrGround=document.getElementById("arrow-ground");
+var arrDown=document.getElementById("arrow-down");
+function notify()
+{
+  arrUp.style.animation="arrNotifyUP 5s";
+  arrGround.style.animation="arrNotifyG 5s";
+  arrDown.style.animation="arrNotifyDn 5s";
+
+}
 var score=0;
+const element = document.getElementById("dispScore"); 
+let t = element.getAttribute("class"); 
+document.getElementsById("pp").innerHTML="fi";
 
-document.getElementsById("dispScore").innerText=1;
-function drag(event) {
-    event.dataTransfer.setData('text',event.target.id);
-  }
-
-  function allow (event) {
-    event.preventDefault();
-  }
-  
-
-  function drop(event) {
-    event.preventDefault();
-    var gol= event.dataTransfer.getData('text');
-    alert("Goal!")
-    document.getElementById("status").innerText="Bravo!";
-
-    score=score+1;
-  }
 
   //----------------- FB sound -------------------------------------
   
   function FBsound_play()
   {
    document.getElementById("FBsound").play();
-   document.getElementById("FBsound-img").src="muted.jpg";
+   document.getElementById("FBsound-img").src='muted.jpg';
   }
 
   
@@ -43,3 +49,5 @@ function drag(event) {
    document.getElementById("FBsound").pause();
    document.getElementById("FBsound-img").src='unMuted.jpg';
   }
+
+
